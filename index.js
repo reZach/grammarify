465c573aea322f3968420ecbd4d00f470d2272f8
reZach <reZach@users.noreply.github.com>
2018-04-22 22:05:46 -0500

    added more cleaning power

--- index.js
+++ index.js
@@ -1,17 +1,71 @@
-exports.test = function (str) {
+var spellchecker = require("spellchecker");
 
-    if (str.length > 0) {
-        
-        // Trim whitespace
-        str = str.trim();
+exports = module.exports = new Grammarify();
 
-        // Capital letter + period
-        str = str[0].toUpperCase() + str.substr(1);
-        if (str[str.length-1] !== "."){
-            str += ".";
+function Grammarify(){
+
+}
+
+Grammarify.prototype.clean = function(str){
+
+    if (str.length === 0){
+        return "";
+    }
+
+    // Get rid of all whitespace
+    var words = str.split(" ");
+    var newWords = words.filter(w => w.length !== 0);
+
+    // Save where there are existing periods
+    var periods = newWords.reduce(function(acc, curr, index){
+        return acc + (curr.indexOf(".") > 0 ? (index.toString() + ".") : "");
+    }, "");
+    periods = periods.split(".");
+    periods = periods.filter(p => p.length !== 0);
+
+    // Clean
+    var replacer = {
+        "bc": "because",
+        "bf": "boyfriend",
+        "gf": "girlfriend",
+        "eg": "example"
+    };
+    var corrections = [];
+    var periodsIndex = -1;
+    for (var i = 0; i < newWords.length; i++){
+
+        // Replace words spellchecker might miss
+        if (typeof replacer[newWords[i]] !== "undefined"){
+            newWords[i] = replacer[newWords[i]];
+        }
+
+        // Spellcheck words
+        if (spellchecker.isMisspelled(newWords[i])){
+            corrections = spellchecker.getCorrectionsForMisspelling(newWords[i]);
+
+            if (corrections.length > 0){
+                newWords[i] = corrections[0];
+                corrections = [];
+            }
+        }
+
+        // Capitalize words if necessary
+        periodsIndex = periods.indexOf((i-1).toString()) >= 0;
+        if (i === 0 || periodsIndex){
+            newWords[i] = newWords[i][0].toUpperCase() + newWords[i].substr(1);
+        }
+
+        // Add period if necessary
+        periodsIndex = periods.indexOf(i.toString()) >= 0;
+        if (periodsIndex || i === newWords.length - 1){
+            newWords[i] = newWords[i] + ".";
+        }
+
+        // Add leading space to word
+        if (i !== 0){
+            newWords[i] = " " + newWords[i];
         }
     }
 
-
-    return str;
+    return newWords.join("");
 }
