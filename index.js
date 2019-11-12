54f8c0e3e1b44e6d8c856614ca5e6300a81eeb21
allen joslin <allen@joslin.net>
2019-11-12 08:49:29 -0500

    Add support for na = n/a becoming N/A
    This is a special case, perhaps not for everyone

--- index.js
+++ index.js
@@ -105,6 +105,9 @@ function Grammarify(){
                 if (i > 0){
                     endingPunctuationIndex = endingPunctuation[i-1] !== "";
                 }                
+                if (newWords[i] === 'n/a') {
+                    newWords[i] = newWords[i].toUpperCase();
+                }
                 if (i === 0 || endingPunctuationIndex){
                     newWords[i] = newWords[i][0].toUpperCase() + newWords[i].substr(1);
                 }
@@ -286,6 +289,7 @@ function Grammarify_SMS(){
 
         // N
         "nite": "night",
+        "na": "n/a",
 
         // O
         "omg": "oh my gosh",
